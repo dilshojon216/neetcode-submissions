21a904b0a7ac06680cf82215a9c3162292801ff8
Dilshodjon <39820449+dilshojon216@users.noreply.github.com>
2026-06-27 23:02:03 +0500

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable=10
+print(type(variable))
+variable=2.2
+print(type(variable))
+variable=True
+print(type(variable))
+variable='serer'
+print(type(variable))
+variable=[1,3,4]
+print(type(variable))
